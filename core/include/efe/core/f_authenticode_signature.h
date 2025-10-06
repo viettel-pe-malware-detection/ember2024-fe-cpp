8f8f2eeda26876dec637d4f8228fcdfdce30cc2a
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-10-07 02:10:52 +0700

    add AuthenticodeSignature and PEFormatWarnings feature types (placeholders only)

---
+++ core/include/efe/core/f_authenticode_signature.h
@@ -0,0 +1,21 @@
+#ifndef EFE_FEATURE_TYPE_AUTHENTICODE_SIGNATURE_INCLUDED
+#define EFE_FEATURE_TYPE_AUTHENTICODE_SIGNATURE_INCLUDED
+
+#include "efe/core/f_base.h"
+
+class AuthenticodeSignature final : public FeatureType {
+public:
+    virtual char const* getName() const override;
+
+    virtual void reset(feature_t* output, PEFile const& peFile) override;
+
+    virtual void start(feature_t* output, PEFile const& peFile) override;
+
+    virtual void reduce(feature_t* output, PEFile const& peFile, size_t bufOffset, uint8_t const* buf, size_t bufSize) override;
+
+    virtual void finalize(feature_t* output, PEFile const& peFile) override;
+
+    virtual size_t getMaxDim() const override;
+};
+
+#endif // EFE_FEATURE_TYPE_AUTHENTICODE_SIGNATURE_INCLUDED
